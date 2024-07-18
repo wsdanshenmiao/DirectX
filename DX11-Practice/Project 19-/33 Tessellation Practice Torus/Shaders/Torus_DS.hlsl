0aed98f0acffad6569c48886a7b3a830628b179e
wsdanshenmiao <wsdanshenmiao@outlook.com>
2024-07-18 11:59:32 +0800

    添加注释

--- DX11-Practice/Project 19-/33 Tessellation Practice Torus/Shaders/Torus_DS.hlsl
+++ DX11-Practice/Project 19-/33 Tessellation Practice Torus/Shaders/Torus_DS.hlsl
@@ -11,22 +11,23 @@ float4 DS(QuadPatchTess patchTess,
     |   |
    3-----2
     */
+    // 获取先前变量
     float3 normal = quad[3].posL;
     float3 origin = quad[4].posL;
     float3 v10 = quad[0].posL - quad[1].posL;
     float3 v12 = quad[2].posL - quad[1].posL;
-    float3 v3 = quad[1].posL + v10 + v12;    // 求第四个点 
-    // 双线性插值
-    float3 v1 = lerp(quad[0].posL, quad[1].posL, uv.x);
-    float3 v2 = lerp(v3, quad[2].posL, uv.x);
-    float3 p = lerp(v1, v2, uv.y);
-
-    // 获取先前变量
     float length = sqrt(pow(v10.x, 2) + pow(v10.y, 2) + pow(v10.z, 2));
     float weight = sqrt(pow(v12.x, 2) + pow(v12.y, 2) + pow(v12.z, 2));
     float torusR = weight / (2 * M_PI);
     float r = length / (2 * M_PI) - torusR;
     float R = 2 * torusR + r;
+    // 求第四个点 
+    float3 v3 = quad[1].posL + v10 + v12;
+
+    // 双线性插值
+    float3 v1 = lerp(quad[0].posL, quad[1].posL, uv.x);
+    float3 v2 = lerp(v3, quad[2].posL, uv.x);
+    float3 p = lerp(v1, v2, uv.y);
 
     float theta = p.y / torusR; // 弧度制
     p = float3(p.x, sin(theta) * torusR, cos(theta) * torusR);  // 面成柱
@@ -39,12 +40,11 @@ float4 DS(QuadPatchTess patchTess,
     theta = acos(normal.y); // 与y轴的夹角
     float lengthxz = sin(theta);    //xz平面投影的长度
     theta = acos(normal.z / lengthxz);
-    float3x3 rotate1 = float3x3(
+    float3x3 rotate1 = float3x3(    // 绕y轴旋转
         float3(cos(theta), 0, -sin(theta)),
         float3(0, 1, 0),
         float3(sin(theta), 0, cos(theta))
     );
-
     // 绕z轴旋转 
     theta = acos(normal.z);
     float lengthxy = sin(theta);
@@ -56,7 +56,6 @@ float4 DS(QuadPatchTess patchTess,
     );
 
     float3x3 rotate = mul(rotate2, rotate1);    // 两次旋转即可
-
     p = mul(rotate, p) + origin;    // 移动到绝对位置需要在域着色器中进行
 
     float4 posH = mul(float4(p, 1.0f), g_WorldViewProj);
--- DX11-Practice/Project 19-/33 Tessellation Practice Torus/Shaders/Torus_HS.hlsl
+++ DX11-Practice/Project 19-/33 Tessellation Practice Torus/Shaders/Torus_HS.hlsl
@@ -3,14 +3,20 @@
 [domain("quad")]
 [partitioning("fractional_even")]
 [outputtopology("triangle_cw")]
-[outputcontrolpoints(5)]
-[patchconstantfunc("QuadConstantHS")]
+[outputcontrolpoints(5)]    //输出5个点
+[patchconstantfunc("QuadConstantHS")]	// 四边形常量着色器
 [maxtessfactor(64.0f)]
 float3 HS( 
 	InputPatch<VertexPosF4, 2> patch, 
 	uint i : SV_OutputControlPointID,
 	uint PatchID : SV_PrimitiveID ) : POSITION
 {
+    /*  length
+   0---------------1
+    |             |
+    |             |weight
+   3---------------2
+    */
     float3 newPoint[5];
 
 	float3 origin = patch[0].posL.xyz;
@@ -21,12 +27,7 @@ float3 HS(
     float torusR = (R - r) / 2; //圆环半径
     float length = 2 * M_PI * (r + torusR);
     float weight = 2 * M_PI * torusR; 
-    /*
-   0-----1
-    |   |
-    |   |
-   3-----2
-    */
+
     newPoint[0] = float3(0, weight, 0);  // 加上原点后为绝对坐标
     newPoint[1] = float3(length, weight, 0);
     newPoint[2] = float3(length, 0, 0);
